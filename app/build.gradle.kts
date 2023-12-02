dd73e65847627f5c05f613c9d1ed7c8c4ef282ee
AkaneChitose <katarina23122000@gmail.com>
2023-12-02 13:28:42 +0700

    third commit Login, Logout, Register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mid_term"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mid_term"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/mid_term/MainActivity.java
+++ app/src/main/java/com/example/mid_term/MainActivity.java
@@ -1,14 +1,40 @@
+// MainActivity.java
+
 package com.example.mid_term;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private Button logoutBtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        logoutBtn = findViewById(R.id.logout_btn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Log out the user
+                auth.signOut();
+
+                // Redirect to the LoginActivity
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish(); // Close the current activity
+            }
+        });
     }
 }
